be6253b860196e507d03bfa768c7e5299e024eea
chezkyk <y0548532123@gmail.com>
2024-07-28 17:07:24 +0300

    added data annotation

--- MVCShoeShop/Models/Friends.cs
+++ MVCShoeShop/Models/Friends.cs
@@ -1,9 +1,20 @@
-﻿namespace MVCShoeShop.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCShoeShop.Models
 {
     public class Friends
     {
+        [Display(Name = "id")]
+        [Required(ErrorMessage ="id is required")]
         public int Id { get; set; }
+
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "name is required")]
         public string Name { get; set; }
+
+        [Display(Name = "age")]
+        [Required(ErrorMessage = "age is required")]
+        [Range(0,120)]
         public int Age { get; set; }
         public Friends() { }
 
